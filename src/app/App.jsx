f07890d74ea155259961204c43df57d0c078a1f1
Freedom Loisa <kitakayaloisa@gmail.com>
2023-03-09 21:10:39 +0300

    changed project structure

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Vite + React</h1>
