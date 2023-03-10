a63f9c293a4d4bdb6ce966dc159798c6e216a8dd
Freedom Loisa <kitakayaloisa@gmail.com>
2023-03-10 11:59:25 +0300

    setting up apollo client

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import App from "./App";
 import store from "./store";
 
 import "./index.css";
 
+const client = new ApolloClient({
+  uri: "http://127.0.0.1:8000/graphql/",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
