0b26cd8efba5d3ad07854a94c7086a47a117f522
Freedom Loisa <kitakayaloisa@gmail.com>
2023-03-14 09:47:43 +0300

    added notification functionality and installed moment.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { Toast } from "primereact/toast";
 import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
 import { ProgressBar } from "primereact/progressbar";
 
 import Footer from "./components/Footer";
@@ -16,8 +17,46 @@ import Dashboard from "./pages/Dashboard";
 const App = () => {
   const notifications = useSelector((state) => state.notification);
   const [isLoading, setIsLoading] = useState(false);
+  const toast = useRef(null);
 
-  const updateNotifications = () => {};
+  const showSuccess = () => {
+    toast.current.show({
+      severity: notifications.type,
+      summary: "Success",
+      detail: notifications.message,
+      life: 3000,
+    });
+  };
+
+  const showInfo = () => {
+    toast.current.show({
+      severity: notifications.type,
+      summary: "Info",
+      detail: notifications.message,
+      life: 3000,
+    });
+  };
+
+  const showError = () => {
+    toast.current.show({
+      severity: notifications.type,
+      summary: "Error",
+      detail: notifications.message,
+      life: 3000,
+    });
+  };
+
+  const updateNotifications = () => {
+    if (notifications.type === "" && notifications.message === "") {
+      // do nothing
+    } else if (notifications.type === "success") {
+      showSuccess();
+    } else if (notifications.type === "info") {
+      showInfo();
+    } else if (notifications.type === "error") {
+      showError();
+    }
+  };
 
   useEffect(() => {
     updateNotifications();
@@ -29,7 +68,7 @@ const App = () => {
       {isLoading && (
         <ProgressBar
           mode="indeterminate"
-          style={{ height: "6px" }}
+          style={{ height: "8px", borderRadius: "0" }}
         ></ProgressBar>
       )}
       {/* loader */}
@@ -62,6 +101,10 @@ const App = () => {
       {/* footer */}
       <Footer />
       {/* footer */}
+
+      {/* toast */}
+      <Toast ref={toast} />
+      {/* toast */}
     </div>
   );
 };
