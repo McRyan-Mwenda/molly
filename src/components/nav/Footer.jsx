ab0d2bbf8d1c6c8dd1c50f4049309444c013bef5
Freedom Loisa <kitakayaloisa@gmail.com>
2023-04-05 10:12:47 +0300

    slightly modified footer

--- src/components/nav/Footer.jsx
+++ src/components/nav/Footer.jsx
@@ -1,18 +1,34 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div className="px-4 py-8 bg-slate-200" id="footer">
       <div className="flex justify-between items-center">
         <p>&copy; 2023 FinanceFluid 💵</p>
-        <p>
-          Made with <i className="bi bi-heart-fill text-red-600"></i> by{" "}
+        <div className="flex items-center">
+          <i class="bi bi-dot"></i>
+          <Link
+            to="/terms&conditions"
+            className="text-blue-600 hover:text-blue-900 mx-1"
+          >
+            Terms & Conditions
+          </Link>
+          <i class="bi bi-dot"></i>
+          <Link
+            to="/sitepolicies"
+            className="text-blue-600 hover:text-blue-900 mx-1"
+          >
+            Site Policies
+          </Link>
+          <i class="bi bi-dot"></i>
+        </div>
+        <div className="flex items-center">
           <a
             href="https://freedomloisa.netlify.app/"
-            className="text-sky-600 hover:text-sky-900 underline"
+            className="text-sky-600 px-2"
           >
-            Freedom Loisa
+            <i class="bi bi-globe2 text-xl"></i>
           </a>
-        </p>
-        <div className="flex items-center">
           <a href="https://github.com/LoisaKitakaya" className="px-2">
             <i className="bi bi-github text-black text-xl"></i>
           </a>
