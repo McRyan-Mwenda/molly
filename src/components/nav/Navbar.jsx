4a34710d59793e945681c75d4e069c97c97893b0
Freedom Loisa <kitakayaloisa@gmail.com>
2023-04-26 11:41:20 +0300

    Frontend design

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,27 +1,59 @@
 const Navbar = () => {
-
   return (
-    <nav className="flex justify-between items-center sticky w-full py-4 px-8">
-      <span className="logo">
-        <a href="">Finance Fluent</a>
+    <nav className="flex justify-between items-center sticky top-0 w-full py-4 px-36 bg-gradient-to-b from-emerald-50 to-white">
+      <span className="font-semibold text-xl">
+        <a href="/">Finance Fluent</a>
       </span>
-      <menu className="flex justify-evenly items-center nav-menu shadow-md">
-        <a href="" className="nav-link">
-          About
-        </a>
-        <a href="" className="nav-link">
-          Solutions
-        </a>
-        <a href="" className="nav-link">
+      <menu className="flex justify-evenly items-center nav-menu">
+        <div className="dropdown">
+          <p className="mx-2 hover:text-emerald-600 cursor-pointer menu-link">
+            Solutions <i className="bi bi-caret-down-fill"></i>
+          </p>
+          <div
+            className="dropdown-content pt-4 px-2 bg-gradient-to-t from-emerald-50 to-white"
+            style={{
+              minWidth: "170px",
+            }}
+          >
+            <a href="" className="hover:text-emerald-600 mt-2 mb-2">
+              Business App
+            </a>
+            <a href="" className="hover:text-emerald-600 mt-2 mb-4">
+              Payment Gateway
+            </a>
+          </div>
+        </div>
+        <div className="dropdown">
+          <p className="mx-2 hover:text-emerald-600 cursor-pointer menu-link">
+            Resources <i className="bi bi-caret-down-fill"></i>
+          </p>
+          <div
+            className="dropdown-content pt-4 px-2 bg-gradient-to-t from-emerald-50 to-white"
+            style={{
+              minWidth: "170px",
+            }}
+          >
+            <a href="" className="hover:text-emerald-600 mt-2 mb-2">
+              Upcoming Events
+            </a>
+            <a href="" className="hover:text-emerald-600 mt-2 mb-2">
+              Blog
+            </a>
+            <a href="" className="hover:text-emerald-600 mt-2 mb-4">
+              FAQs
+            </a>
+          </div>
+        </div>
+        <a href="/pricing" className="mx-2 hover:text-emerald-600">
           Pricing
         </a>
-        <a href="" className="nav-link">
-          Resources
+        <a href="/about" className="mx-2 hover:text-emerald-600">
+          About
         </a>
-        <a href="" className="nav-link">
+        <a href="/contacts" className="mx-2 hover:text-emerald-600">
           Contacts
         </a>
-        <a href="" className="nav-link app-button">
+        <a href="" className=" app-button">
           Go to App
         </a>
       </menu>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,54 @@ const Home = () => {
 
   return (
     <div className="page">
-      <h1 className="text-3xl">Home Page</h1>
+      <div className="mx-36 flex justify-between items-center my-28">
+        <div
+          style={{
+            width: "425px",
+          }}
+        >
+          <h1
+            className="text-emerald-600"
+            style={{
+              fontSize: "48px",
+            }}
+          >
+            We help you grow and do more
+          </h1>
+          <br />
+          <p>
+            We don't just deliver, we also pick, store, pack and offer payment
+            processing.
+          </p>
+          <br />
+          <p>
+            We power you to sell more, move goods efficiently and affordably.
+          </p>
+          <br />
+          <a
+            href=""
+            className="px-4 py-2 border rounded-md bg-emerald-500 hover:bg-emerald-600 nav-link text-zinc-50 hover:text-white"
+          >
+            Book a demo
+          </a>
+        </div>
+        <iframe
+          width="575"
+          height="350"
+          className="shadow-md"
+          src="https://www.youtube.com/embed/VBHr0faDCoQ"
+          title="Bebe Rexha &amp; Snoop Dogg - Satellite (Official Music Video)"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div className="items-center mx-36">
+        <h4 className="text-emerald-600 text-2xl font-semibold text-center">
+          Unlocking opportunities for businesses
+        </h4>
+        
+      </div>
     </div>
   );
 };
